df51a558198c403d29bfdb7575e0e99b7112432f
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-07-25 16:05:29 +0300

    Initial Commit

---
+++ src/main/java/com/jrt/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.jrt.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!!");
+
+        return "foo";
+    }
+}
